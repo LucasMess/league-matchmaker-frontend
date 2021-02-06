5297c4e0b97a1f969c61e3c56332a4f45333242e
Lucas Message <lucas.message@yahoo.com>
2021-02-06 09:35:50 -0500

    Test config

--- karma.conf.js
+++ karma.conf.js
@@ -25,8 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadlessNoSandbox'],
+    singleRun: true,
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+        ]
+      }
+    }
   });
 };
